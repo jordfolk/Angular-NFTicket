fa6c6e12804736266d5e3229b5977dc89b730f53
Jordfolk <mj.elborough@hotmail.com>
2022-03-10 12:25:34 +0100

    Upload file 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventsComponent } from './events/events.component';
-import { EventInfoComponent } from './event-info/event-info.component';
-import { LoginComponent } from './login/login.component';
-import { SaleComponent } from './sale/sale.component';
-import { EventUserComponent } from './event-user/event-user.component';
-import { AdminComponent } from './admin/admin.component';
+import { EventsComponent } from './routes/events/events.component';
+import { EventInfoComponent } from './routes/event-info/event-info.component';
+import { LoginComponent } from './routes/login/login.component';
+import { SaleComponent } from './routes/sale/sale.component';
+import { EventUserComponent } from './routes/event-user/event-user.component';
+import { AdminComponent } from './routes/admin/admin.component';
 
 @NgModule({
   declarations: [
