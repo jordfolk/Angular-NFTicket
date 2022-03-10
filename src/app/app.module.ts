3a085aeb54f9480bc67cedd54f6d3d9b9defb5af
Jordfolk <mj.elborough@hotmail.com>
2022-03-10 11:48:37 +0100

    Upload files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { EventInfoComponent } from './event-info/event-info.component';
+import { LoginComponent } from './login/login.component';
+import { SaleComponent } from './sale/sale.component';
+import { EventUserComponent } from './event-user/event-user.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    EventInfoComponent,
+    LoginComponent,
+    SaleComponent,
+    EventUserComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
